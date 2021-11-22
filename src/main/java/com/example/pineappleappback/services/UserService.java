4890c6d5055550c70931e12ec8f6f566100ba0fc
Alejandra Adrian Tejada <thalia.adrian@tecsup.edu.pe>
2021-11-22 09:22:04 -0500

    encript password

--- src/main/java/com/example/pineappleappback/services/UserService.java
+++ src/main/java/com/example/pineappleappback/services/UserService.java
@@ -13,6 +13,8 @@ import com.example.pineappleappback.response.ResponseHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service // le indico que es el servicio
@@ -21,7 +23,13 @@ public class UserService {
      private UserRepository userRepository;
      @Autowired
      private RoleRepository roleRepository;
+     
 
+    static PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+     public static String encriptarBCrypt(String password) {
+          return  passwordEncoder.encode(password);
+     }
+     
      public ResponseEntity<Object> getUsers() { // para obtener todos los usuarios
           try {
                ArrayList<UserModel> users = new ArrayList<UserModel>();
@@ -62,7 +70,7 @@ public class UserService {
                newUser.setName(user.getName());
                newUser.setEmail(user.getEmail());
                newUser.setLastName(user.getLastName());
-               newUser.setPassword(user.getPassword());
+               newUser.setPassword(encriptarBCrypt(user.getPassword()));
                newUser.setUsername(user.getUsername());
                newUser.setCreatedDateToNow();
                newUser.setRole(existingRole.get());
