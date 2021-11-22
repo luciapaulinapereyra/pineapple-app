4b885a21eeec525e2d31b0e4bd06f432d24e5885
Alejandra Adrian Tejada <thalia.adrian@tecsup.edu.pe>
2021-11-22 06:55:37 -0500

    change userListDTO

--- src/main/java/com/example/pineappleappback/dto/UserListDTO.java
+++ src/main/java/com/example/pineappleappback/dto/UserListDTO.java
@@ -3,17 +3,26 @@ package com.example.pineappleappback.dto;
 
 import java.util.Date;
 
+import com.example.pineappleappback.models.RoleModel;
 import com.example.pineappleappback.models.UserModel;
 
 public class UserListDTO {
     private String username;
     private Long id;
     private Date createdDate;
+    private String name;
+    private String lastname;
+    private String email;
+    private RoleModel role;
     
     public UserListDTO(UserModel user) {
         this.username = user.getUsername();
         this.id = user.getId();
         this.createdDate = user.getCreatedDate();
+        this.name = user.getName();
+        this.lastname = user.getLastName();
+        this.email = user.getEmail();
+        this.role = user.getRole();
 
     }
 
@@ -41,6 +50,32 @@ public class UserListDTO {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }   
 
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }   
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }   
+
+    public RoleModel getRole() {
+        return role;
+    }
+    public void setRole(RoleModel role) {
+        this.role = role;
+    }   
 }
